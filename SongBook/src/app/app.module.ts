3d54df313ea171fd96c908f57ac923d2e771d3bb
Paul O'Connor <jamespauloconnor@gmail.com>
2017-07-28 13:10:41 -0400

    Component set up

--- SongBook/src/app/app.module.ts
+++ SongBook/src/app/app.module.ts
@@ -3,9 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header.component';
+import { BandsComponent } from './bands/bands.component';
+import { BandListComponent } from './bands/band-list/band-list.component';
+import { BandDetailComponent } from './bands/band-detail/band-detail.component';
+import { BandItemComponent } from './bands/band-list/band-item/band-item.component';
+import { SongListComponent } from './song-list/song-list.component';
+import { SongEditComponent } from './song-list/song-edit/song-edit.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BandsComponent,
+    BandListComponent,
+    BandDetailComponent,
+    BandItemComponent,
+    SongListComponent,
+    SongEditComponent
   ],
   imports: [
     BrowserModule
