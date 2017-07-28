f49765a544b8d7cc2f1e5123dad8b34aceef3d94
Paul O'Connor <jamespauloconnor@gmail.com>
2017-07-28 14:32:28 -0400

    Created band model

---
+++ SongBook/src/app/bands/band.model.ts
@@ -0,0 +1,15 @@
+//No decorator. Just vanilla TS.
+//Blueprint for objects that we create.
+
+export class Band{
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name: string, desc: string, imagePath: string){
+        this.name = name;
+        this.description = desc;
+        this.imagePath = imagePath;
+
+    }
+}
