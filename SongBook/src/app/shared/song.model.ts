b842ba74ef358758f646077bc2f997d4ac09fb2e
Paul O'Connor <jamespauloconnor@gmail.com>
2017-07-29 09:59:48 -0400

    Added Song model, song edit component, and song-list component

---
+++ SongBook/src/app/shared/song.model.ts
@@ -0,0 +1,7 @@
+//No decorator. Just vanilla TS.
+//Blueprint for objects that we create.
+
+//amount in this case is number of times practiced.
+export class Song{
+    constructor(public name: string, public amount:number){ }
+}
