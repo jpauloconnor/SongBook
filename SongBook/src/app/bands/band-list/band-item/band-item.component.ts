87752464075fecb2765fdb4b6aeaea4415ac4651
Paul O'Connor <jamespauloconnor@gmail.com>
2017-10-02 23:45:31 -0400

    Small adjustments to get back on track

--- SongBook/src/app/bands/band-list/band-item/band-item.component.ts
+++ SongBook/src/app/bands/band-list/band-item/band-item.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Band } from '../../band.model';
 @Component({
   selector: 'app-band-item',
   templateUrl: './band-item.component.html',
   styleUrls: ['./band-item.component.css']
 })
 export class BandItemComponent implements OnInit {
-
+  @Input() band: Band;
   constructor() { }
 
   ngOnInit() {
--- SongBook/src/app/bands/band-list/band-list.component.ts
+++ SongBook/src/app/bands/band-list/band-list.component.ts
@@ -9,7 +9,7 @@ import { Band } from '../band.model';
 export class BandListComponent implements OnInit {
 
   bands: Band[] = [
-    new Band("Pink Floyd", "Greatest band ever.", "http://cdn.wallpapersafari.com/34/36/a2lSYG.jpg"),
+    new Band("Led Zeppelin", "Greatest band ever.", "https://commons.wikimedia.org/wiki/File:Grave_JohnBonham_sept07.JPG#/media/File:Grave_JohnBonham_sept07.JPG"),
     new Band("Pink Floyd", "Greatest band ever.", "http://cdn.wallpapersafari.com/34/36/a2lSYG.jpg"),
     new Band("Pink Floyd", "Greatest band ever.", "http://cdn.wallpapersafari.com/34/36/a2lSYG.jpg"),
     new Band("Pink Floyd", "Greatest band ever.", "http://cdn.wallpapersafari.com/34/36/a2lSYG.jpg")
